5c7840b64a151b86d793326d89c5e90cba49de48
Rolf <relekang@gmail.com>
2012-03-13 17:24:35 +0100

    more work on dbhandler
    added mysql driver in libs

--- project/src/no/ntnu/fp/storage/DbHandler.java
+++ project/src/no/ntnu/fp/storage/DbHandler.java
@@ -5,15 +5,20 @@ import no.ntnu.fp.model.Room;
 
 import java.sql.*;
 import java.util.ArrayList;
+import java.util.Properties;
 
 public class DbHandler {
     private Connection conn;
     private String username;
     private String password;
+
+    private String mysqlDriver;
+    private String url;
     
-    public DbHandler(){
-        username = "fp";
-        password = "6pKp0hXPMN";
+    public DbHandler() throws SQLException {
+        username = "sondrele_fp";
+        password = "fellesp1";
+        url="mysql://mysql.stud.ntnu.no";
     }
     
     public DbHandler(String username, String password){
@@ -23,25 +28,17 @@ public class DbHandler {
     
     public boolean connect() throws SQLException {
         try {
-            String url = "jdbc:mysql://lkng.me/fp";
-            Class.forName("com.mysql.jdbc.Driver").newInstance();
-            conn = DriverManager.getConnection(url, username, password);
-            System.out.println ("Database connection established");
-            return true;
+            Class.forName("com.mysql.jdbc.Driver");
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
         }
-        catch (Exception e) {
-            System.err.println ("Cannot connect to database server");
-        }
-        finally {
-            if (conn != null){
-                try {
-                    conn.close ();
-                    System.out.println ("Database connection terminated");
-                } catch (Exception e) { /* ignore close errors */ }
-            }
-        }
-        return false;
+        Properties props = new Properties();
+        props.setProperty("user", username);
+        props.setProperty("password", password);
+        conn = DriverManager.getConnection(url, props);
+        return true;
     }
+
 
     public void close() throws SQLException {
         conn.close();
@@ -60,9 +57,10 @@ public class DbHandler {
                 event.setDateFrom(rs.getDate("date_from"));
                 event.setDateTo(rs.getDate("date_to"));
                 event.setRoom(new Room(rs.getInt("room_id")));
-                event.setDescription(rs.getString("description"));
+                event.setDescription(rs.getString(" description"));
                 events.add(event);
             }
+            rs.close();
             close();
             return events;
 
@@ -73,10 +71,40 @@ public class DbHandler {
 
     }
 
+    public Event fetchEvent(String arg){
+        try {
+            if(!connect())
+                return null;
+            Statement stmt = conn.createStatement();
+            ResultSet rs = stmt.executeQuery("SELECT * FROM EVENT WHERE " + arg);
+            Event event;
+            while (rs.next()) {
+                event = new Event(rs.getString("title"));
+                event.setDateFrom(rs.getDate("date_from"));
+                event.setDateTo(rs.getDate("date_to"));
+                event.setRoom(new Room(rs.getInt("room_id")));
+                event.setDescription(rs.getString(" description"));
+
+            }
+            rs.close();
+            close();
+
+            return null;
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return null;
+        }
+
+    }
+
 
     public static void main (String args[]){
-        DbHandler dbHandler = new DbHandler();
+        try {
+            DbHandler dbHandler = new DbHandler();
         System.out.print(dbHandler.fetchAllEvents().size());
+        } catch (SQLException e){
+
+        }
     }
 
 
