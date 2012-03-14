27d19f0c9c6fdd10eeddf193209d2fda60a840c0
Shouryu <instagibno@gmail.com>
2012-03-14 16:06:25 +0100

    fikser participant renderer, nA ser ikonene bra ut - til en viss grad.

--- project/src/no/ntnu/fp/gui/ParticipantRenderer.java
+++ project/src/no/ntnu/fp/gui/ParticipantRenderer.java
@@ -1,13 +1,16 @@
 package no.ntnu.fp.gui;
 
+import java.awt.Color;
 import java.awt.Component;
 import no.ntnu.fp.model.Employee;
 import no.ntnu.fp.model.Employee.Status;
 
 import javax.swing.DefaultListCellRenderer;
 import javax.swing.ImageIcon;
+import javax.swing.JButton;
 import javax.swing.JLabel;
 import javax.swing.JList;
+import javax.swing.JPanel;
 import javax.swing.ListCellRenderer;
 
 public class ParticipantRenderer extends DefaultListCellRenderer implements ListCellRenderer{
@@ -16,28 +19,34 @@ public class ParticipantRenderer extends DefaultListCellRenderer implements List
 	public Component getListCellRendererComponent(JList list, Object value,
 			int index, boolean isSelected, boolean cellHasFocus){
 				
-		JLabel print = (JLabel) super.getListCellRendererComponent(list, value, index, isSelected, cellHasFocus);
-		
-		
+		//JLabel print = (JLabel) super.getListCellRendererComponent(list, value, index, isSelected, cellHasFocus);
+		JPanel panel = new JPanel();
+		JLabel label = new JLabel();
+		JButton button = new JButton();
 		if(((Employee) value).getAccepted() == Status.ACCEPTED){
 			java.net.URL imgURL = getClass().getResource("/resources/icons/accept.png");
 			ImageIcon icon = new ImageIcon(imgURL);
-			print.setIcon(icon);
+			panel.setBackground(Color.BLACK); 
+			
+			
+
+			setVisible(true); 
+			
+			label.setIcon(icon);
+			label.setText( ((Employee) value).getName());	
+			label.setForeground(Color.GREEN);			
+			panel.add(label);
+			
 	
 		}
-		if(((Employee) value).getAccepted() == Status.DECLINED){
-			java.net.URL imgURL = getClass().getResource("/resources/icons/declined.png");
-			ImageIcon icon = new ImageIcon(imgURL);
-			print.setIcon(icon);
-			
-		}
+
 		
-		print.setText( ((Employee) value).getName());	
 		java.net.URL delIMG = getClass().getResource("/resources/icons/delete.png");
 		ImageIcon delete = new ImageIcon(delIMG);
-		print.setIcon(delete);
-		
-		return this;
+		button.setIcon(delete);
+		button.setBackground(Color.black);
+		panel.add(button);
+		return panel;
 		
 	}
 	
