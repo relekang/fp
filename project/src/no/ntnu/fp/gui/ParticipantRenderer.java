f21d761933733b23a9e812eade9a0d1a788f0f39
Shouryu <instagibno@gmail.com>
2012-03-14 17:05:17 +0100

    la inn pending status i listrenderer

--- project/src/no/ntnu/fp/gui/ParticipantRenderer.java
+++ project/src/no/ntnu/fp/gui/ParticipantRenderer.java
@@ -2,6 +2,9 @@ package no.ntnu.fp.gui;
 
 import java.awt.Color;
 import java.awt.Component;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
 import no.ntnu.fp.model.Employee;
 import no.ntnu.fp.model.Employee.Status;
 
@@ -13,7 +16,7 @@ import javax.swing.JList;
 import javax.swing.JPanel;
 import javax.swing.ListCellRenderer;
 
-public class ParticipantRenderer extends DefaultListCellRenderer implements ListCellRenderer{
+public class ParticipantRenderer extends JPanel implements ListCellRenderer{
 	
 	@Override
 	public Component getListCellRendererComponent(JList list, Object value,
@@ -39,15 +42,55 @@ public class ParticipantRenderer extends DefaultListCellRenderer implements List
 			
 	
 		}
-
+		else if(((Employee) value).getAccepted() == Status.DECLINED){
+			java.net.URL imgURL = getClass().getResource("/resources/icons/decline.png");
+			ImageIcon icon = new ImageIcon(imgURL);
+			panel.setBackground(Color.BLACK); 
+			
+			
+			
+			setVisible(true); 
+			
+			label.setIcon(icon);
+			label.setText( ((Employee) value).getName());	
+			label.setForeground(Color.RED);			
+			panel.add(label);
+			
+			
+		}
+		else{
+			java.net.URL imgURL = getClass().getResource("/resources/icons/pending.png");
+			ImageIcon icon = new ImageIcon(imgURL);
+			panel.setBackground(Color.BLACK); 
+			
+			
+			
+			setVisible(true); 
+			
+			label.setIcon(icon);
+			label.setText( ((Employee) value).getName());	
+			label.setForeground(Color.ORANGE);			
+			panel.add(label);
+			
+		}
 		
 		java.net.URL delIMG = getClass().getResource("/resources/icons/delete.png");
 		ImageIcon delete = new ImageIcon(delIMG);
 		button.setIcon(delete);
 		button.setBackground(Color.black);
+		button.addActionListener(new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				System.out.println("moooo");
+				
+			}
+		});
 		panel.add(button);
 		return panel;
 		
 	}
+
+	
 	
 }
