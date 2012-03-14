fa02a17eac84b258517a6f4d221d3a96f16c99a9
Shouryu <instagibno@gmail.com>
2012-03-14 11:30:06 +0100

    lagde javadoc for notification.java

--- project/src/no/ntnu/fp/model/Notification.java
+++ project/src/no/ntnu/fp/model/Notification.java
@@ -3,18 +3,34 @@ package no.ntnu.fp.model;
 
 public class Notification {
     private String description;
+    
+    /**
+     * Constructor for Notification
+     * @param desc
+     */
     public Notification(String desc){
         description = desc;
     }
-
+    
+    /**
+     * returns the description string
+     * @return description
+     */
     public String getDescription() {
         return description;
     }
-
+    
+    /**
+     * Sets a string for the description
+     * @param description
+     */
     public void setDescription(String description) {
         this.description = description;
     }
-
+    
+    /**
+     * toString method, prints out the description.
+     */
     @Override
     public String toString() {
         return getDescription();
