6303975c0bc51526517bb086667e218c19e05aff
Shouryu <instagibno@gmail.com>
2012-03-20 17:01:50 +0100

    popup menu - eventview

--- project/src/no/ntnu/fp/gui/EventView.java
+++ project/src/no/ntnu/fp/gui/EventView.java
@@ -13,6 +13,7 @@ import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JComboBox;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JList;
 import javax.swing.JPanel;
 import javax.swing.JPopupMenu;
