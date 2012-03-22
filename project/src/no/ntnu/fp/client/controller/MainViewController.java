661a7e3921525e3ad2ccdb073d0b29e48118c05e
Rolf <relekang@gmail.com>
2012-03-22 20:11:23 +0100

    Check if user is logged in before fetching events

--- project/src/no/ntnu/fp/client/controller/MainViewController.java
+++ project/src/no/ntnu/fp/client/controller/MainViewController.java
@@ -12,43 +12,45 @@ import no.ntnu.fp.common.model.Event;
 import no.ntnu.fp.server.storage.db.EventHandler;
 
 public class MainViewController implements PropertyChangeListener {
-	
-	private MainView mainView;
-	private Employee currentUser;
-	private ArrayList<Event> events;
-	
-	public MainViewController(MainView view) {
-		mainView = view;
-	}
-	
-	public void setCurrentUser(Employee currentUser){
-		this.currentUser = currentUser;
-		currentUser.addPropertyChangeListener(this);
-		loadUserEvents();
-		loadUserNotifications();
-	}
-	
-	public void loadUserEvents() {
-		events = Connection.fetchEvents();
-		System.out.println("EVENT:" + events.get(0));
-		mainView.getCalendarPanel().addEvents(events);
-	}
-	
-	public void loadUserNotifications() {
-		
-	}
-	
-	public Employee getCurrentUser() {
-		return currentUser;
-	}
 
-	@Override
-	public void propertyChange(PropertyChangeEvent evt) {
-		System.out.println("propertychange in MainViewController: " + evt);
-	}
+    private MainView mainView;
+    private Employee currentUser;
+    private ArrayList<Event> events;
 
-	public MainView getMainView() {
-		return mainView;
-	}
-	
+    public MainViewController(MainView view) {
+        mainView = view;
+    }
+
+    public void setCurrentUser(Employee currentUser) {
+        this.currentUser = currentUser;
+        currentUser.addPropertyChangeListener(this);
+        loadUserEvents();
+        loadUserNotifications();
+    }
+
+    public void loadUserEvents() {
+        if (currentUser != null) {
+            events = Connection.fetchEvents();
+            System.out.println("EVENT:" + events.get(0));
+            mainView.getCalendarPanel().addEvents(events);
+        }
+    }
+
+    public void loadUserNotifications() {
+
+    }
+
+    public Employee getCurrentUser() {
+        return currentUser;
+    }
+
+    @Override
+    public void propertyChange(PropertyChangeEvent evt) {
+        System.out.println("propertychange in MainViewController: " + evt);
+    }
+
+    public MainView getMainView() {
+        return mainView;
+    }
+
 }
