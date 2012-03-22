176b7e4be56a60b92152137586ee7f7cbcc8c8c0
Shouryu <instagibno@gmail.com>
2012-03-22 14:37:13 +0100

    lagde getters i datelabel

--- project/src/no/ntnu/fp/gui/objects/DateLabel.java
+++ project/src/no/ntnu/fp/gui/objects/DateLabel.java
@@ -11,6 +11,7 @@ import java.util.Date;
 public class DateLabel extends JLabel implements MouseListener{
     private Calendar cal;
     private int month;
+    
 
     public DateLabel(Date date, int month){
         super();
@@ -25,10 +26,24 @@ public class DateLabel extends JLabel implements MouseListener{
         }
         this.addMouseListener(this);
     }
-
+    
+    public int getYear(){
+    	return cal.get(Calendar.YEAR);
+    	
+    }
+    public int getMonth(){
+    	return cal.get(Calendar.MONTH);
+    	
+    }
+    public int getDay(){
+    	return cal.get(Calendar.DATE);
+    	
+    }
+    
     @Override
     public void mouseClicked(MouseEvent mouseEvent) {
         System.out.println(cal.get(Calendar.YEAR)+"-"+(cal.get(Calendar.MONTH)+1)+"-"+cal.get(Calendar.DAY_OF_MONTH));
+        
     }
 
     @Override
