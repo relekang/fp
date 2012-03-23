b8cfd113a3c8ff5e793d74d3d09c57007f6b0e95
Sondre Lefsaker <leffsa@gmail.com>
2012-03-23 18:23:52 +0100

    par småendringer

--- project/src/no/ntnu/fp/client/controller/ClientApplication.java
+++ project/src/no/ntnu/fp/client/controller/ClientApplication.java
@@ -1,10 +1,8 @@
 package no.ntnu.fp.client.controller;
 
-import no.ntnu.fp.client.Connection;
 import no.ntnu.fp.client.gui.MainView;
 import no.ntnu.fp.client.gui.FindPersonView;
 import no.ntnu.fp.common.model.Employee;
-import no.ntnu.fp.common.model.Event;
 
 import javax.swing.*;
 
@@ -18,7 +16,7 @@ public class ClientApplication {
     private static FindPersonViewController findPersonViewController = new FindPersonViewController();
     
     public static void main (String args[]) throws ClassNotFoundException, InstantiationException, IllegalAccessException, UnsupportedLookAndFeelException{
-//    	UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+    	UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
     	mainFrame = new MainView();
         mainFrame.setLocationRelativeTo(null);
         mainFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
